5366a100454dffb3bae9936d3d28c9873af571d1
aseelps <aseelpsathar@gmail.com>
2021-05-13 11:46:48 +0530

    juice: Bringup Pe
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,17 +6,15 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Dot stuff.
-WITH_GAPPS := true
-$(call inherit-product, vendor/dot/config/common.mk)
+# Inherit some common Pe stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_BLUR := true
-DOT_OFFICIAL := true
+
 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := dot_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -25,3 +23,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
+
