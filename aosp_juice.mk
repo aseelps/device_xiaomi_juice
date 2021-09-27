008d21cea202e399f9234530d3dba30bb130aa8e
aseelps <aseelpsathar@gmail.com>
2021-09-27 05:50:34 +0000

    juice: init pe+

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,15 +6,16 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Dot stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+# Inherit some common pe stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := dot_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
